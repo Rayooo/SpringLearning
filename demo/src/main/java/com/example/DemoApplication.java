c991e0b49c4a50c87e2f19a05aa4e477c34c35a4
Rayooo <chenxiaolei.ray@gmail.com>
2016-07-03 20:04:34 +0800

    实现登陆

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,67 @@
 package com.example;
 
+import demo.UiApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	@RequestMapping("/user")
+	public Principal user(Principal user){
+		return user;
+	}
+
+
+	@RequestMapping("/resource")
+	public Map<String,Object> home(){
+		Map<String,Object> model = new HashMap<String,Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content","Hello Kitty");
+		return model;
+	}
+
+	public static void main(String[] args){
+		SpringApplication.run(UiApplication.class, args);
+	}
+
+	//    @Configuration
+//    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+//    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+//        @Override
+//        protected  void configure(HttpSecurity http) throws Exception{
+//            http
+//                    .httpBasic().
+//                    and()
+//                    .authorizeRequests()
+//                    .antMatchers("/index.html", "/home.html","login.html","/")
+//                    .permitAll()
+//                    .anyRequest()
+//                    .authenticated();
+//        }
+//    }
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.httpBasic()
+					.and()
+					.authorizeRequests()
+					.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+					.anyRequest().authenticated();
+		}
 	}
 }
--- demo/target/classes/static/js/hello.js
+++ demo/target/classes/static/js/hello.js
@@ -1,13 +1,71 @@
 /**
  * Created by Ray on 16/7/2.
  */
-angular.module('hello', [])
+angular.module('hello', ['ngRoute'])
+    .config(function ($routeProvider, $httpProvider) {
+
+        $routeProvider.when('/',{
+            templateUrl: 'home.html',
+            controller: 'home',
+            controllerAs: 'controller'
+        }).when('/login',{
+            templateUrl: 'login.html',
+            controller: 'navigation',
+            controllerAs: 'controller'
+        }).otherwise('/');
+
+        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+
+    })
+
+
     .controller('home',function ($scope,$http) {
         // $scope.greeting = {
         //     id: 'Ray',
         //     content: 'hello world'
         // }
-        $http.get("/resource/").then(function (response) {
+        $http.get("/user").then(function (response) {
             $scope.greeting = response.data;
         })
+    })
+
+    .controller('navigation', function ($rootScope, $http, $location) {
+        var self = this;
+        var authenticate = function (credentials, callback) {
+            var headers = credentials? {authorization: "Basic" + btoa(credentials.username + ":" + credentials.password)} : {};
+            $http.get('/user',{headers:headers}).then(function (response) {
+                if(response.data.name){
+                    $rootScope.authenticated = true;
+                }
+                else{
+                    $rootScope.authenticated = false;
+                }
+                callback && callback();
+            }, function () {
+                $rootScope.authenticated = false;
+                callback  && callback();
+            });
+        };
+        authenticate();
+        self.credentials = {};
+
+        self.login = function () {
+            authenticate(self.credentials,function () {
+                if($rootScope.authenticated){
+                    $location.path("/");
+                    self.error = false;
+                }else{
+                    $location.path("login");
+                    self.error = true;
+                }
+            });
+        }
+
+        self.logout = function () {
+            $http.post('/logout',{}).finally(function () {
+                $rootScope.authenticated = false;
+                $location.path("/");
+            })
+        }
+
     })
